2da3a3fa2d3b5429661c7b06a1fb45b53f0e089b
tranhuyducseven <tranhuyducseven@gmail.com>
2022-06-12 23:01:59 +0700

    [Day1-ex1] Done!

--- Day1/ex1_sub_array/src/main.rs
+++ Day1/ex1_sub_array/src/main.rs
@@ -1,4 +1,22 @@
 use std::io;
+
+fn is_sub_array(parent_array: Vec<String>, sub_array: Vec<String>) -> bool {
+    let mut i = 0;
+    let mut j = 0;
+    while i < parent_array.len() && j < sub_array.len() {
+        if parent_array[i] == sub_array[j] {
+            i += 1;
+            j += 1;
+            if j == sub_array.len() {
+                return true;
+            };
+        } else {
+            i = i - j + 1;
+            j = 0;
+        }
+    }
+    return false;
+}
 // fn print_type_of<T>(_: &T) {
 //     println!("{}", std::any::type_name::<T>())
 // }
@@ -13,17 +31,22 @@ fn input_array(name_array: &str) -> Vec<String> {
     let number_of_array: usize = buffer.trim().parse().unwrap();
     buffer.clear();
     let mut arr = vec![String::new(); number_of_array];
-    for x in 0..number_of_array{        
-        println!("{}_arr[{}]: ",name_array, x);
+    for x in 0..number_of_array {
+        println!("{}_arr[{}]: ", name_array, x);
         io::stdin().read_line(&mut buffer).expect("Need a number");
         arr[x] = buffer.trim().into();
         buffer.clear();
-    };
+    }
     arr
-   
 }
 
 fn main() {
     let parent_array: Vec<String> = input_array("parent");
-    let sub_array: Vec<String> = input_array("sub");    
+    let sub_array: Vec<String> = input_array("sub");
+    if is_sub_array(parent_array, sub_array) {
+        println!("Yes, it is a sub array")
+    }
+    else{
+        println!("No, it isn't")
+    }
 }
