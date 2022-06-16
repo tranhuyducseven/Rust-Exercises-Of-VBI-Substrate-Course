2ad84c0f778bde6a722ffdc93ce1de987068f103
tranhuyducseven <tranhuyducseven@gmail.com>
2022-06-16 09:17:32 +0700

    [Done] done ex2

---
+++ Day2/ex2/target/debug/deps/ex2-487917538af34630.d
@@ -0,0 +1,5 @@
+/media/D/Learning/Rust/Rust-Exercises-Of-VBI-Substrate-Course/Day2/ex2/target/debug/deps/ex2-487917538af34630: src/main.rs
+
+/media/D/Learning/Rust/Rust-Exercises-Of-VBI-Substrate-Course/Day2/ex2/target/debug/deps/ex2-487917538af34630.d: src/main.rs
+
+src/main.rs:
---
+++ Day2/ex2/target/debug/ex2.d
@@ -0,0 +1,1 @@
+/media/D/Learning/Rust/Rust-Exercises-Of-VBI-Substrate-Course/Day2/ex2/target/debug/ex2: /media/D/Learning/Rust/Rust-Exercises-Of-VBI-Substrate-Course/Day2/ex2/src/main.rs
